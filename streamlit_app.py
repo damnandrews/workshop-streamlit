6fe5f7445ff616bea7783188a8ae676888d67dc7
damnandrews <oliveiraa.andree@gmail.com>
2022-07-23 11:44:18 -0300

    Teste1

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,11 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
 import plotly.express as px
+
+st.title('Visualização do gapminder')
+
+
 df = px.data.gapminder()
 fig = px.scatter(df.query("year==2007"), x="gdpPercap", y="lifeExp", size="pop", color="continent",
            hover_name="country", log_x=True, size_max=60)
